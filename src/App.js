fba7c7020540cd052a06f351c2b05c7540530bb1
Ilya Komar <helias@yandex.ru>
2021-11-22 16:28:10 +0300

    refactor: add API

--- src/App.js
+++ src/App.js
@@ -2,31 +2,25 @@ import { useEffect, useState } from 'react';
 import { Search } from './components/search/Search';
 import { Recipe } from './components/recipe/Recipe';
 import { Preloader } from './components/preloader/Preloader';
+import { getRecipes } from './api/api';
 import css from './app.module.css';
 
 const App = () => {
-
-  const APP_ID = '2a78c8d0';
-  const APP_KEY = '971dadb5aa39f928622f72c64518dfa7';
-
-  const REQUEST = `https://api.edamam.com/api/recipes/v2?type=public&q=Pepperoni&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
   const [recipes, setRecipes] = useState([]);
   const [isPreloaderVisible, setPreloaderVisible] = useState(false);
 
   useEffect( () => {
-    getRecipes(setPreloaderVisible);
+    showRecipes(setPreloaderVisible);
   }, [] );
 
-  const getRecipes = async (setPreloaderVisible) => {
+  const showRecipes = async (setPreloaderVisible) => {
     setPreloaderVisible(true);
-    const response = await fetch(REQUEST);
-    const recipes = await response.json();
+    const recipes = await getRecipes('spaghetti');
     setRecipes(recipes.hits);
     setPreloaderVisible(false);
     console.log(recipes);
   }
-
   
   return (
     <div className="App">
