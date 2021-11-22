5cb2ce4523c251dfbb42f71724a438c058632bc9
Ilya Komar <helias@yandex.ru>
2021-11-22 18:28:41 +0300

    feat: show error if query length is shorter than 5 symbols

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,6 @@ const APP_KEY = '971dadb5aa39f928622f72c64518dfa7';
 export const getRecipes = async (query = 'pepperoni') => {
   try {
     const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-    debugger;
     const recipes = await response.json();
     return recipes;
   }
--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,14 +1,49 @@
+import { useState } from 'react';
 import css from './search.module.scss';
 
 export const Search = () => {
 
+  const INPUT_ERROR_1 = 'Search query mast be at least 3 symbols';
+
+  const [inputValue, setInputValue] = useState('');
+  const [formErrors, setFormErrors] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.length < 5) {
+      setFormErrors([...formErrors, INPUT_ERROR_1])
+    }
+  }
+
+  const handleInput = (e) => {
+    setInputValue(e.target.value);
+    setFormErrors([]);
+  }
+
   return (
     <div className={css.item}>
-      <form className="searchForm">
-        <input type="text" placeholder="search recipe" className={css.item__searchInput} />
-        <button type="submit" className={css.item__searchSubmit}>
-          Search
-        </button>
+      <form onSubmit={handleSubmit}>
+        <div className={css.item__box}>
+          <div className={css.item__boxItem1}>
+            <input
+              type="text"
+              placeholder="search recipe"
+              className={css.item__searchInput}
+              value={inputValue}
+              onChange={handleInput}
+            />
+            {!!formErrors.length && (
+              <p className={css.item__error}>
+                {formErrors.map((error, index) => <span className={css.item__errorText} key={index}>{error}</span>)}
+              </p>
+            )}
+          </div>
+          <div>
+            <button type="submit" className={css.item__searchSubmit}>
+              Search
+            </button>
+          </div>
+        </div>
       </form>
     </div>
   )
