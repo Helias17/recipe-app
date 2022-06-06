dd2d163da15a6f59b538a4dd3c7268e2ebf751f0
Ilya Komar <helias@yandex.ru>
2022-06-06 20:25:00 +0300

    deploy v1

--- src/components/preloader/Preloader.jsx
+++
@@ -1,6 +0,0 @@
-import css from "./preloader.module.scss";
-import preloaderImage from "../../images/loading-buffering.gif";
-
-export const Preloader = () => {
-  return <img src={preloaderImage} alt="" className={css.item} />;
-};
